b0b9397993e51582f570825440dbf961c66fe048
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2026-08-27 06:07:39 +0100

    seperated pages

---
+++ script.js
@@ -0,0 +1,13 @@
+const reveals = document.querySelectorAll(".reveal");
+const io = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((e) => {
+			if (e.isIntersecting) {
+				e.target.classList.add("in");
+				io.unobserve(e.target);
+			}
+		});
+	},
+	{ threshold: 0.12 },
+);
+reveals.forEach((el) => io.observe(el));
